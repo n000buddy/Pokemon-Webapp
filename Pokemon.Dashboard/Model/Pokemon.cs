2e0094ac20fd0041402974f512c751183c1ab83f
T <tknaupp@outlook.de>
2025-05-04 21:26:51 +0200

    chore: add Filter and Design

--- Pokemon.Dashboard/Model/Pokemon.cs
+++ Pokemon.Dashboard/Model/Pokemon.cs
@@ -6,7 +6,10 @@ public class Pokemon
     public int id { get; set; }
     public int height { get; set; }
     
+    public int base_experience { get; set; }
+    public bool is_default { get; set; }
     public int weight  { get; set; }
+    public int order {get; set;}
     
     public PokemonSprites? sprites { get; set; }
     
