5187cf26dae7305f59b7b0a97ad15217c5984177
Tobias Knaupp <tobias.knaupp@cgi.com>
2025-05-06 09:22:37 +0200

    chore: Fix Formatting

--- Pokemon.Dashboard/Model/Pokemon.cs
+++ Pokemon.Dashboard/Model/Pokemon.cs
@@ -5,6 +5,6 @@ public class Pokemon
     public string? name { get; set; }
     public int id { get; set; }
     public int height { get; set; }
-    public int weight  { get; set; } 
+    public int weight { get; set; }
     public PokemonSprites? sprites { get; set; }
 }
