b580d3b9444e0c2dcf1c6d7b8a3a6cc05a221584
T <tknaupp@outlook.de>
2025-05-11 19:20:44 +0200

    chore: Add-and-change-card

--- Pokemon.Dashboard/Model/Pokemon.cs
+++ Pokemon.Dashboard/Model/Pokemon.cs
@@ -5,10 +5,31 @@ public class Pokemon
     public string? name { get; set; }
     public int id { get; set; }
     public int height { get; set; }
-    
-    public int weight  { get; set; }
-    
-    public PokemonSprites? sprites { get; set; }
-    
-    
+
+    public int weight { get; set; }
+    // public int hp { get; set; } // HP hinzufügen
+     public PokemonSprites? sprites { get; set; }
+     public List<PokemonAbility>? abilities { get; set; } // Fähigkeiten hinzufügen
+     public List<PokemonType>? types { get; set; } // Typen hinzufügen
+
+}
+
+public class PokemonAbility
+{
+    public Ability? ability { get; set; }
+}
+
+public class Ability
+{
+    public string? name { get; set; }
+}
+
+public class PokemonType
+{
+    public Type? type { get; set; }
+}
+
+public class Type
+{
+    public string? name { get; set; }
 }
