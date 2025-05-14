0757725d0639859d2679e1ead6081b0990afcff0
T <tknaupp@outlook.de>
2025-05-14 07:41:17 +0200

    chore: changes on the Card

--- Pokemon.Dashboard/Model/NamedApiResource.cs
+++ Pokemon.Dashboard/Model/NamedApiResource.cs
@@ -4,4 +4,5 @@ public class NamedApiResource
 {
     public string? name { get; set; }
     public string? url { get; set; }   
+    
 }
--- Pokemon.Dashboard/Model/NamedApiResourceList.cs
+++ Pokemon.Dashboard/Model/NamedApiResourceList.cs
@@ -3,8 +3,7 @@ namespace Pokemon.Dashboard.Model;
 public class NamedApiResourceList
 {
     public int count { get; set; }
-    public string? next {get;set;}
-    public string? previous {get;set;}
+    public string? next { get; set; }
+    public string? previous { get; set; }
     public List<NamedApiResource>? results { get; set; }
-    
 }
